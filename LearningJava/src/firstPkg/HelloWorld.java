964e070586f360ab1aa69d5abe1526ac097edc18
NarenInfy <narendra_p02@infosys.com>
2018-09-11 21:32:00 +1000

    initial change
    Change-Id: I0000000000000000000000000000000000000001
    Signed-off-by: kpnarendra@aol.com <kpnarendra@aol.com>

--- LearningJava/src/firstPkg/HelloWorld.java
+++ LearningJava/src/firstPkg/HelloWorld.java
@@ -13,7 +13,8 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Java program");
+		System.out.println("Java program.");
+		System.out.println("Github integration");
 	}
 
 }
