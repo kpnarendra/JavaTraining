896b251f6e4708a0e5328046d999833002b4c161
kpnarendra@aol.com <kpnarendra@aol.com>
2018-09-13 08:28:32 +1000

    commit again

--- LearningJava/src/firstPkg/HelloWorld.java
+++ LearningJava/src/firstPkg/HelloWorld.java
@@ -13,8 +13,8 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("Following subjects are covered so far.");
-		System.out.println("\t" + i++ + " . " + "Java and JDK installation");
-		System.out.println("\t" + i++ + " . " +"Github integration");
+		System.out.println("\t " + i++ + " . " + "Java and JDK installation");
+		System.out.println("\t " + i++ + " . " +"Github integration");
 	}
 	
 }
