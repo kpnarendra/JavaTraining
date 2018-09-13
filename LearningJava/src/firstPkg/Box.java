a7b37178d7c96d76552d9584978a33c23b6023cd
kpnarendra@aol.com <kpnarendra@aol.com>
2018-09-13 22:35:17 +1000

    Learning Classes and Methods.
    Change-Id: I0000000000000000000000000000000000000004
    Signed-off-by: kpnarendra@aol.com <kpnarendra@aol.com>

---
+++ LearningJava/src/firstPkg/Box.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package firstPkg;
+
+/**
+ * @author narendrakp
+ *
+ */
+public class Box {
+	double width;
+	double height;
+	double depth;
+}
---
+++ LearningJava/src/firstPkg/PropertiesOfClass.java
@@ -0,0 +1,33 @@
+package firstPkg;
+
+public class PropertiesOfClass {
+
+	Box myBox, hisBox, myBoxDup;
+
+	public void printProprtiesOfClass() {
+
+		System.out.println("myBox @ 1 = " + myBox);
+
+		myBox = new Box();
+		
+		myBoxDup = myBox;
+		
+
+		System.out.println("myBox @ 2 = " + myBox);
+		System.out.println("myBoxDup @ 3 = " + myBoxDup);
+		System.out.println("myBox and myBoxDup are same have myBox.equals(myBoxDup) is : "+myBox.equals(myBoxDup));
+		System.out.println("hisBox @ 4 =" + hisBox);
+
+		System.out.println("myBox reference : " + myBox.getClass());
+		System.out.println("Super class of any class : " + myBox.getClass().getSuperclass().toString());
+
+		try {
+			System.out.println("hisBox reference : " + hisBox.getClass());
+			System.out.println("Super class of any class : " + hisBox.getClass().getSuperclass().toString());
+
+		} catch (NullPointerException ex) {
+			System.err.println("NullPointerExceptionFound for object : hisBox \n" + ex);
+		}
+	}
+
+}
